1bcd71c0a3fb5d440406d243536bafedf0db95ea
CHJUNE-android <chjune0205@gmail.com>
2020-11-15 17:45:13 +0900

    [로또 과제 완료] 요구사항 최대한 맞춰보려 했는데 테스트코드가 자신이 없네요.. LottoInspectorTest.kt 에서 이렇게 테스트 하는게 좀 애매하다는 생각이 드네요..ㅎㅎ ktlint적용완료

--- src/main/kotlin/lotto/view/WinningInputView.kt
+++ src/main/kotlin/lotto/view/WinningInputView.kt
@@ -14,9 +14,7 @@ class WinningInputView {
     private fun inputInspect(winnerNums: List<String>) {
         require(winnerNums.size == 6) {
             throw IllegalArgumentException(
-                "당첨 번호를 잘못 입력하셨습니다. \n " +
-                        "입력값: $winnerNums \n " +
-                        "개수와 띄어쓰기에 유의하여 다음과 같이 입력해주세요 : 1, 2, 5, 7, 14, 23"
+                "당첨 번호를 잘못 입력하셨습니다. \n 입력값: $winnerNums \n 개수와 띄어쓰기에 유의하여 다음과 같이 입력해주세요 : 1, 2, 5, 7, 14, 23"
             )
         }
     }
