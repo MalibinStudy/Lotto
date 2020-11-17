6331d0277a83c5ad626ee1278ebf1f570bbfdfe4
RoadtoS7 <kshvv975@gmail.com>
2020-11-18 01:48:07 +0900

    test(LottoCashierTest.kt): add LottoCashierTest

---
+++ src/test/kotlin/lotto/domain/LottoCashierTest.kt
@@ -0,0 +1,27 @@
+package lotto.domain
+
+import lotto.domain.LottoCashier.buyLotto
+import lotto.domain.LottoCashier.calculateLottoCount
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class LottoCashierTest {
+    @ParameterizedTest
+    @CsvSource("1000, 1", "10000, 10")
+    fun `금액으로부터 사려는 로또의 개수를 올바르게 반환하는지 테스트`(money: Int, expectedLottoCount: Int) {
+        // when
+        val lottoCount = calculateLottoCount(money)
+        assertThat(lottoCount).isEqualTo(expectedLottoCount)
+    }
+
+    @ParameterizedTest
+    @CsvSource("1", "7")
+    fun `로또를 로또머신으로부터 뽑은 것이 알맞은 개수로 출력되었는지 확인하기`(lottoCount: Int) {
+        // when
+        val lottos = buyLotto(lottoCount)
+
+        // then
+        assertThat(lottos.size).isEqualTo(lottoCount)
+    }
+}
