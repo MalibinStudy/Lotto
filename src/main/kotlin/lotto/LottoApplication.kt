b7af1d6d1b1b0278060c97a9c815c914198d7d3e
RoadtoS7 <kshvv975@gmail.com>
2020-11-18 01:47:31 +0900

    feat(LottoApplication.kt): add LottoApplication.kt

---
+++ src/main/kotlin/lotto/LottoApplication.kt
@@ -0,0 +1,26 @@
+package lotto
+
+import lotto.domain.Lotto
+import lotto.domain.LottoCashier
+import lotto.domain.LottoScoringMachine
+import lotto.view.InputView.receiveMoney
+import lotto.view.InputView.receiveWinLottoNumbers
+import lotto.view.OutputView.printLottoCount
+import lotto.view.OutputView.printLottoScore
+import lotto.view.OutputView.printLottos
+import lotto.view.OutputView.printProfitRate
+
+fun main() {
+    val money = receiveMoney()
+    val lottoCount = LottoCashier.calculateLottoCount(money)
+    val lottos = LottoCashier.buyLotto(lottoCount)
+    printLottoCount(lottoCount)
+    printLottos(lottos)
+
+    val winLotto = Lotto(receiveWinLottoNumbers())
+    val lottoScore = LottoScoringMachine.scoreLottos(winLotto, lottos)
+    printLottoScore(lottoScore)
+
+    val profitRate = lottoScore.getProfitRate(money)
+    printProfitRate(profitRate)
+}
