287f937cd09874588b0f225eb9bf1b9431ebf0d1
RoadtoS7 <kshvv975@gmail.com>
2020-11-18 01:46:23 +0900

    feat(WinLottoScore.kt): add WinLottoScore
    사용자가 산 로또들이 얼마나 많이 맞았는지 총 정보를 저장하고 있는 WinLottoScore 클래스를 만듬

---
+++ src/main/kotlin/lotto/domain/WinLottoScore.kt
@@ -0,0 +1,32 @@
+package lotto.domain
+
+import java.lang.IllegalArgumentException
+import kotlin.math.roundToInt
+
+class WinLottoScore(
+    private val score: MutableMap<WinLotto, Int> = mutableMapOf(
+        WinLotto.THREE to 0,
+        WinLotto.FOUR to 0,
+        WinLotto.FIVE to 0,
+        WinLotto.SIX to 0,
+    )
+) {
+    fun getLottoCount(numberWinNumberCount: WinLotto): Int {
+        return score[numberWinNumberCount] ?: throw IllegalArgumentException("입력은 LottoWinCount의 인스턴스여야 합니다.")
+    }
+
+    fun increaseLottoCount(numberWinNumberCount: WinLotto) {
+        score[numberWinNumberCount] = score[numberWinNumberCount]!!.inc()
+    }
+
+    fun getProfitRate(money: Int): Double {
+        val profiteRate = getTotalPrize() / money.toDouble()
+        return (profiteRate * 100).roundToInt() / 100.0
+    }
+
+    private fun getTotalPrize(): Int {
+        val totalPrize =
+            WinLotto.THREE.prizeMoney * score[WinLotto.THREE]!! + WinLotto.FOUR.prizeMoney * score[WinLotto.FOUR]!! + WinLotto.FIVE.prizeMoney * score[WinLotto.FIVE]!! + WinLotto.SIX.prizeMoney * score[WinLotto.SIX]!!
+        return totalPrize
+    }
+}
