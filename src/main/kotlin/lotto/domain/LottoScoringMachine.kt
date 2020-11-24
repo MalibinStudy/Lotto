89a89d2148e8b3b35c4cf7ff564e4b776fa275e7
RoadtoS7 <kshvv975@gmail.com>
2020-11-24 21:42:10 +0900

    refactor(LottoScoringMachine.kt): fix to use LottoTicket

--- src/main/kotlin/lotto/domain/LottoScoringMachine.kt
+++ src/main/kotlin/lotto/domain/LottoScoringMachine.kt
@@ -3,20 +3,15 @@ package lotto.domain
 object LottoScoringMachine {
     private const val LEAST_WIN_NUMBER = 3
 
-    fun scoreLottos(winLotto: Lotto, lottos: List<Lotto>): WinLottoScore {
-        val lottoWinScore = WinLottoScore()
+    fun scoreLottos(winLottoTicket: LottoTicket, lottoTickets: List<LottoTicket>): WinLottoStatistics {
+        val lottoWinScore = WinLottoStatistics()
 
-        lottos.asSequence()
-            .map { lotto -> getSameNumberCount(winLotto, lotto) }
+        lottoTickets.asSequence()
+            .map { it.countSameNumber(winLottoTicket) }
             .filter { it >= LEAST_WIN_NUMBER }
             .map { WinLotto.findInstance(it) }
             .map { lottoWinScore.increaseLottoCount(it) }
             .toList()
-
         return lottoWinScore
-    }
-
-    private fun getSameNumberCount(winLotto: Lotto, lotto: Lotto): Int {
-        return lotto.numbers.count { winLotto.checkHas(it) }
     }
 }
