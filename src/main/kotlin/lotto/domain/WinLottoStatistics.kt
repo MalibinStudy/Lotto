b3f2c902150445bf368bdba01da2f4935d4c45b0
RoadtoS7 <kshvv975@gmail.com>
2020-11-26 14:30:27 +0900

    refactor(WinLottoTicket.kt): rename WinLotto to WinLottoTicket.kt

--- src/main/kotlin/lotto/domain/WinLottoStatistics.kt
+++ src/main/kotlin/lotto/domain/WinLottoStatistics.kt
@@ -4,21 +4,21 @@ import java.util.EnumMap
 import kotlin.math.roundToInt
 
 class WinLottoStatistics(
-    private val count: EnumMap<WinLotto, Int> = EnumMap(WinLotto::class.java)
+    private val count: EnumMap<WinLottoTicket, Int> = EnumMap(WinLottoTicket::class.java)
 ) {
     init {
-        count[WinLotto.THREE] = 0
-        count[WinLotto.FOUR] = 0
-        count[WinLotto.FIVE] = 0
-        count[WinLotto.SIX] = 0
+        count[WinLottoTicket.THREE] = 0
+        count[WinLottoTicket.FOUR] = 0
+        count[WinLottoTicket.FIVE] = 0
+        count[WinLottoTicket.SIX] = 0
     }
 
-    fun getLottoCount(numberWinNumberCount: WinLotto): Int {
-        return count[numberWinNumberCount] ?: throw IllegalArgumentException("입력은 WinLotto의 인스턴스여야 합니다.")
+    fun getLottoCount(numberWinNumberCountTicket: WinLottoTicket): Int {
+        return count[numberWinNumberCountTicket] ?: throw IllegalArgumentException("입력은 WinLotto의 인스턴스여야 합니다.")
     }
 
-    fun increaseLottoCount(numberWinNumberCount: WinLotto) {
-        count[numberWinNumberCount] = count[numberWinNumberCount]!!.inc()
+    fun increaseLottoCount(numberWinNumberCountTicket: WinLottoTicket) {
+        count[numberWinNumberCountTicket] = count[numberWinNumberCountTicket]!!.inc()
     }
 
     fun getProfitRate(money: Money): Double {
