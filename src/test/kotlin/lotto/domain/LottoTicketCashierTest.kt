847afa01dca2fa4ab6e4c5fbbcc7968ad75b2646
RoadtoS7 <kshvv975@gmail.com>
2020-11-26 14:32:50 +0900

    refactor(LottoTicket.kt): refact LottoTicketCashierTest
    LottoTicketCashier를 object에서 class로 만들면서 LottoTicketCashierTest에서
    LottoTicketCashier를 객체로 갖도록 함

--- src/test/kotlin/lotto/domain/LottoTicketCashierTest.kt
+++ src/test/kotlin/lotto/domain/LottoTicketCashierTest.kt
@@ -1,17 +1,19 @@
 package lotto.domain
 
-import lotto.domain.LottoCashier.getLotto
-import lotto.domain.LottoCashier.calculateLottoCount
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
-class LottoCashierTest {
+class LottoTicketCashierTest {
+    private val lottoCashier = LottoCashier { 1 }
+
     @ParameterizedTest
     @CsvSource("1000, 1", "10000, 10")
     fun `금액으로부터 사려는 로또의 개수를 올바르게 반환하는지 테스트`(money: Int, expectedLottoCount: Int) {
+        // given
+        val money = Money(money)
         // when
-        val lottoCount = calculateLottoCount(money)
+        val lottoCount = lottoCashier.calculateLottoCount(money)
         assertThat(lottoCount).isEqualTo(expectedLottoCount)
     }
 
@@ -19,7 +21,7 @@ class LottoCashierTest {
     @CsvSource("1", "7")
     fun `로또를 로또머신으로부터 뽑은 것이 알맞은 개수로 출력되었는지 확인하기`(lottoCount: Int) {
         // when
-        val lottos = getLotto(lottoCount)
+        val lottos = lottoCashier.getLotto(lottoCount)
 
         // then
         assertThat(lottos.size).isEqualTo(lottoCount)
