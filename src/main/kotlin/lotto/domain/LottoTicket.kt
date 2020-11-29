a611238a89720d160dcaa5be7ffea12f4e1a3583
RoadtoS7 <kshvv975@gmail.com>
2020-11-29 21:46:32 +0900

    refact(LottoTicket.kt): make LottoTicket have hasLottoNumber, LOTTO_PRICE
    하나의 LottoNumber를 가지고 있는지 체크하는 함수를 만듬
    LOTTO하나의 가격을 상수로 갖도록 함

--- src/main/kotlin/lotto/domain/LottoTicket.kt
+++ src/main/kotlin/lotto/domain/LottoTicket.kt
@@ -1,27 +1,36 @@
 package lotto.domain
 
-data class LottoTicket(val numbers: List<LottoNum>) {
+data class LottoTicket(private val lottoNumbers: List<LottoNumber>) {
 
     init {
         checkNumberCount()
+        checkDuplication()
     }
 
     private fun checkNumberCount() {
-        require(numbers.size == LOTTO_TICKET_LENGTH) { "로또 숫자는 총 6개입니다." }
+        require(lottoNumbers.size == LOTTO_TICKET_LENGTH) { "로또 숫자는 총 6개입니다." }
+    }
+
+    private fun checkDuplication() {
+        val lottoNumberSet = lottoNumbers.toSet()
+        require(lottoNumberSet.size == LOTTO_TICKET_LENGTH) { "로또 숫자는 중복될 수 없습니다." }
     }
 
     fun countSameNumber(lottoTicket: LottoTicket): Int {
-        val thisLottoTicketNumbers = this.numbers.map { it.num }
-        val lottoTicketNumbers = lottoTicket.numbers.map { it.num }
+        val thisLottoTicketNumbers = this.lottoNumbers.map { it.number }
+        val lottoTicketNumbers = lottoTicket.lottoNumbers.map { it.number }
         return lottoTicketNumbers.count { thisLottoTicketNumbers.contains(it) }
     }
 
+    fun hasLottoNumber(lottoNumber: LottoNumber): Boolean = lottoNumbers.contains(lottoNumber)
+
     override fun toString(): String {
-        val numbers = numbers.map { it.num }
+        val numbers = lottoNumbers.map { it.number }
         return numbers.toString()
     }
 
     companion object {
         const val LOTTO_TICKET_LENGTH = 6
+        const val LOTTO_PRICE = 1000
     }
 }
