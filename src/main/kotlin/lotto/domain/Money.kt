ed7f09d54b02c64dd30322f19b5fbab1517def3d
RoadtoS7 <kshvv975@gmail.com>
2020-11-29 21:52:29 +0900

    refact(Money): make Money calculate affordable goods count

--- src/main/kotlin/lotto/domain/Money.kt
+++ src/main/kotlin/lotto/domain/Money.kt
@@ -4,4 +4,8 @@ class Money(val value: Int) {
     init {
         require(value >= 0) { "돈은 항상 양수입니다." }
     }
+
+    fun getAffordableGoodsCount(goodsPrice: Int): Int {
+        return this.value / goodsPrice
+    }
 }
