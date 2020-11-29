a97f5cae7fb62cbfad268ef678522770e50ac17e
RoadtoS7 <kshvv975@gmail.com>
2020-11-29 21:45:01 +0900

    refact(LottoNumberCache.kt): make LottoNumCache move to LottoNumber

--- src/main/kotlin/lotto/domain/LottoNumCache.kt
+++
@@ -1,9 +0,0 @@
-package lotto.domain
-
-object LottoNumCache {
-    private val cache: HashMap<Int, LottoNum> = hashMapOf()
-
-    fun getLottoNum(lottoNum: Int): LottoNum {
-        return cache.getOrPut(lottoNum, { LottoNum(lottoNum) })
-    }
-}
